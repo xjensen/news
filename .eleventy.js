e497d056671a11828123baec54397b31f46b25a0
Jon Jensen <github@id.xjensen.com>
2020-11-05 21:50:49 -0800

    Move CMS files

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy({ "src/css": "css" });
   eleventyConfig.addPassthroughCopy({ "src/img": "img" });
   eleventyConfig.addPassthroughCopy({ "src/CNAME": "/" });
+  eleventyConfig.addPassthroughCopy({ "src/cms": "cms" });
 
   eleventyConfig.addCollection("mySort", function(collection) {
     let posts = [];
